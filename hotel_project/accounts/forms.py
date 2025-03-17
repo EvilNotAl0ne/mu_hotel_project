87dafaf2baf56ea258da79c212c3075e591a4967
EvilNotAl0ne <sacha1234sacha@mail.ru>
2025-03-17 21:43:12 +0300

    17.03

--- hotel_project/accounts/forms.py
+++ hotel_project/accounts/forms.py
@@ -1,10 +1,23 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm): # Клас каторый содержит встроеную форму имя и пароль
+
+class RegisterForm(UserCreationForm): # Класс каторый содержит встроеную форму имя и пароль
     email = forms.EmailField(required=True)  # Добовляем еще одну форму уже в готовую с именим и поролем
 
     class Meta: # Указываем что наша форма будет работать с БД
         model = User
         fields = ['username', 'email', 'password1', 'password2'] # Определения спика полей в БД
+
+class LoginForm(AuthenticationForm): # Класс каторый содержит встроеную форму аунтефикации
+    username = forms.CharField(
+        label="Имя пользователя", # Отоброжает надпись рядом с полем ввода
+        widget=forms.TextInput(attrs={'placeholder': 'Введите имя пользователя'}) # TextInput то что это текстовое поле
+    )
+    password = forms.CharField(
+        label="Пароль",  # Отоброжает надпись рядом с полем ввода
+        widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'})  # PasswordInput то что это текстовое поле ввода пороля
+    )
+
+
--- hotel_project/accounts/views.py
+++ hotel_project/accounts/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import RegisterForm
+from django.contrib.auth import login, authenticate
+# authenticate - проверяет существует ли такой пользователь
+# login - остовляет аутефецированым пользователя
+from .forms import RegisterForm, LoginForm
 
 def register(request):
-    if request.method == 'POST':  # Провереям был ли POST запрос
+    if request.method == 'POST':  # Проверяем был ли POST запрос
         form = RegisterForm(request.POST)
-        if form.is_valid():  # Провереям была ли коректно заполнина наша форма
-            user = form.save()  # Если все ок, то сохроняем
+        if form.is_valid():  # Проверяем была ли коректно заполнена наша форма
+            user = form.save()  # Если все ок, то сохраняем
             login(request, user)  # Если так же все ок, то авторизуем пользователя
-            return redirect('home') # И перемещаем его на главную строницу
+            return redirect('home') # И перемещаем его на главную страницу
 
     else:
-        form = RegisterForm() # Если метод GET то просто отоброжаем пустую форму для заполнения
+        form = RegisterForm() # Если метод GET, то просто отображаем пустую форму для заполнения
 
     return render(request, 'accounts/register.html', {'form': form})  # Это путь к нашему шаблону HTML для отображения визуального вида нашей формы
 
 
+def user_login(request):
+    if request.method == 'POST':  # Проверяет заполнена ли форма да или нет
+        form = LoginForm(request, data=request.POST)  # Если да, то заполняет нашу форму
+        if form.is_valid():        # Проверяет валидацию, все ли прошло, все ли заполнено
+            username = form.cleaned_data['username']  # Если все ок, то извлекает наши запоненые данные
+            password = form.cleaned_data['password']  # Тут то же самое!
+            user = authenticate(request, username=username, password=password) # Функция authenticate проверяет если такой пользователь, если да то возвращает его, если нет то возвращает None
+            if user is not None:  # Если пользователь Не равен None
+                login(request, user) # То авторизует его
+                return redirect('home') # И отправляет его на главную страницу
+
+    else:
+        form = LoginForm()  # Если метод GET, то просто отображаем пустую форму для заполнения
+
+    return render(request, 'accounts/login.html', {'form': form})  # Это путь к нашему шаблону HTML для отображения визуального вида нашей формы
